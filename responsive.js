a25a632906848284f5e6a0ca7e0a3a86e6a00615
Rajat Tanwar <122817532+rajatanwar00@users.noreply.github.com>
2023-08-13 14:58:31 +0530

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,29 @@
+
+
+function response(){
+    
+let tablen=document.querySelector('#expense-table');
+
+let ename=document.querySelector('#name');
+let edate=document.querySelector('#date');
+let eamount=document.querySelector('#amount');
+
+
+    let adname=ename.value;
+    let addate=edate.value;
+    let adamount=eamount.value;
+
+    let template= `
+                    <tr>
+                        <td>${adname}</td>
+                        <td>${addate}</td>
+                        <td>${adamount}</td>
+                    </tr>`;
+
+    tablen.innerHTML +=template;
+    
+    ename.value="";
+    edate.value="";
+    eamount.value="";
+}
+
